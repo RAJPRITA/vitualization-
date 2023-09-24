45cae2c107f1e31c864d53d3a6721a9be214d5ee
PRITAM RAJ <pritamrajbps@gmail.com>
2023-09-24 22:48:00 +0530

    recent canges

--- pizzaOrder.py
+++ pizzaOrder.py
@@ -1,19 +1,35 @@
-print("Welcome to pizza order dilivary")
+from flask import Flask, render_template, request
 
-size = input("What is the size of piza you need ? S,M,L")
-add_pepporoni = input("Do you want pepporoni? Y OR N")
-extra_cheese = input("do you want extra cheese")
+app = Flask(__name__)
 
-if size == "S":
-    bill = 15
-elif size == "M":
-    bill = 20
-else:
-    bill = 25
+# Define pizza prices in a dictionary
+pizza_prices = {
+    "S": 15,
+    "M": 20,
+    "L": 25
+}
 
-if add_pepporoni =="Y":
-    bill +=2
-if extra_cheese == "Y":
-    bill +=1
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        size = request.form.get('size', '').strip().upper()
+        add_pepperoni = request.form.get('pepperoni', '').strip().upper()
+        extra_cheese = request.form.get('extraCheese', '').strip().upper()
 
-print(f"Your final bill is {bill}")
+        # Validate user input for size
+        if size not in pizza_prices:
+            return "Invalid pizza size selected."
+
+        bill = pizza_prices[size]
+
+        if add_pepperoni == "Y":
+            bill += 2
+        if extra_cheese == "Y":
+            bill += 1
+
+        return f"Your final bill is ${bill}"
+
+    return render_template('order.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
